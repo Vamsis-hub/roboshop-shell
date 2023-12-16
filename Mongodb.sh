ff347723728e3f2cce689a63f1ce450c79028867
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-16 19:06:28 +1000

    Roboshop shell

--- Mongodb.sh
+++ Mongodb.sh
@@ -14,10 +14,10 @@ Logfile="/tmp/$0-$TIME.log"
 Validate(){
 if [ $1 -ne 0 ]
 then
-    echo -e "$2 .....$R Failed $N"  &>>$Logfile
+    echo -e "$2 .....$R Failed $N"  
     exit 1
 else 
-    echo -e "$2.....$G Success $N"  &>>$Logfile
+    echo -e "$2.....$G Success $N"  
 
 fi
 

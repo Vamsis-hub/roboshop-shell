9dab5ffaf413e60efc1adbfa4fa6ee268ad54984
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-16 18:59:04 +1000

    Roboshop shell

--- Mongodb.sh
+++ Mongodb.sh
@@ -6,13 +6,18 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+
+TIME=$(date +%F-%H-%M-%S)
+
+Logfile="/tmp/$0-$TIME.log"
+
 Validate(){
 if [ $1 -ne 0 ]
 then
-    echo -e "$2 .....$R Failed $N" 
+    echo -e "$2 .....$R Failed $N"  >>&$Logfile
     exit 1
 else 
-    echo -e "$2.....$G Success $N" 
+    echo -e "$2.....$G Success $N"  >>&$Logfile
 
 fi
 
@@ -20,14 +25,36 @@ fi
 
 if [ $ID -ne 0 ]
 then 
-   echo -e "$R Error,please run with root user $N" 
+   echo -e "$R Error,please run with root user $N"  >>&$Logfile
    exit 1
 else 
-   echo -e "$G I am a root user $N"  
+   echo -e "$G I am a root user $N"   >>&$Logfile
 fi
 
-cp Mongo.repo /etc/yum.repos.d/mongo.repo 
+cp Mongo.repo /etc/yum.repos.d/mongo.repo  >>&$Logfile
 
 Validate $? "COPING OF Mongorepo"
+
+dnf install mongodb-org -y  >>&$Logfile
+
+Validate $? "installation of mongodb"
+
+systemctl enable mongod >>&$Logfile
+
+Validate $? "enabling of mongodb"
+
+systemctl start mongod >>&$Logfile
+
+Validate $? "starting of mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf >>&$Logfile
+
+Validate $? "enabling remote access"
+
+systemctl restart mongod >>&$Logfile
+
+Validate $? "restart of mongodb"
+
+
 
 

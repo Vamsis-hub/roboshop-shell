328f3a7ba8b3a761ad7a5afc1c7958348d8718c3
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-20 07:51:54 +1000

    Roboshop shell

---
+++ roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INSTANCES=("MONGODB "REDIS" "MYSQL" "RABBITMQ" "CART" "USER" "CATALOGUE" "PAYMENT" "SHIPPING" "DISPATCH" "WEB")
+
+
+if i in "${INSTANCES[@]}"
+do
+    if [ $i == "MONGODB" ] || [ $i == "REDIS" ] || [ $i == "MYSQL" ]
+    then
+         INSTANCE_TYPE="t3.small"
+    else
+         INSTANCE_TYPE="t2.micro"
+
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba
+
+done
+

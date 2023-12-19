8714cac34928763f51ce21c2f146691497cd08a5
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-20 07:59:49 +1000

    Roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -10,6 +10,7 @@ do
          INSTANCE_TYPE="t3.small"
     else
          INSTANCE_TYPE="t2.micro"
+    fi
 
     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba
 

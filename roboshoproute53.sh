f9391a1f7f63471aa0e9a7603a9a0c2e5424ef1b
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-20 13:49:47 +1000

    Roboshop shell

--- roboshoproute53.sh
+++ roboshoproute53.sh
@@ -9,11 +9,11 @@ do
      INSTANCE_TYPE=t3.small
   else
      INSTANCE_TYPE=t2.micro
-fi
+  fi
 
    IPADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb   --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
-    echo "$i:$IPADDRESS"
+    echo "$i: $IPADDRESS"
 
 done
 

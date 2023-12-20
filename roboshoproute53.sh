37a8af74de2c1cb4e05156c1b7e559513e5b2fef
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-20 13:39:44 +1000

    Roboshop shell

--- roboshoproute53.sh
+++ roboshoproute53.sh
@@ -13,7 +13,7 @@ do
 
    IPADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb   --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
-    echo "$i=$IPADDRESS"
+    echo "$i:$IPADDRESS"
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id Z07247122IIE0GLRLIHLG \
@@ -23,11 +23,11 @@ do
         ,"Changes": [{
         "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-            "Name"              : "$i.'saanvi.website'"
+            "Name"              : "$i'.'saanvi.website'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : '"$IPADDRESS"'
+                "Value"         : "'$IPADDRESS'"
             }]
         }
         }]

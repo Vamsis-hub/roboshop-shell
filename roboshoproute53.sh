6ba3e80e4628c1f78dcb8fb46b71e4748030e577
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-20 13:31:25 +1000

    Roboshop shell

--- roboshoproute53.sh
+++ roboshoproute53.sh
@@ -11,7 +11,7 @@ do
      INSTANCE_TYPE=t2.micro
   fi
 
-   IPADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+   IPADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb   --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
     echo "$i=$IPADDRESS"
 
@@ -23,7 +23,7 @@ do
         ,"Changes": [{
         "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'saanvi.website'"
+            "Name"              : "$i.'saanvi.website'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{

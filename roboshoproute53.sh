02ede9a3382175d669c42ae351bc253d9ab4dc2e
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-20 13:52:16 +1000

    Roboshop shell

--- roboshoproute53.sh
+++ roboshoproute53.sh
@@ -13,7 +13,7 @@ do
 
    IPADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb   --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
-    echo "$i= $IPADDRESS"
+    echo $i= $IPADDRESS
 
 done
 

accdef8545c5d1d7afbb8d0177fbf98090b4c97a
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-20 13:48:42 +1000

    Roboshop shell

--- roboshoproute53.sh
+++ roboshoproute53.sh
@@ -7,9 +7,9 @@ do
   if [ $i == "MONGODB" ] || [ $i == "SHIPPING" ] || [ $i == "MYSQL" ]
   then 
      INSTANCE_TYPE=t3.small
-   else
+  else
      INSTANCE_TYPE=t2.micro
-  fi
+fi
 
    IPADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb   --instance-type $INSTANCE_TYPE --security-group-ids sg-080a5a1150d6297ba --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
